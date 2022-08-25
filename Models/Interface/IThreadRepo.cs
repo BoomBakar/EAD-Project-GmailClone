49ed0ae691414b6c3a9912a5097bb41214514d86
BoomBakar <abubakardar608@gmail.com>
2022-08-25 20:34:41 +0500

    Interfaces of repos made

---
+++ Models/Interface/IThreadRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Models.Interface
+{
+    public interface IThreadRepo
+    {
+        public Thread getThreadInfo(int id);
+        public bool AddThread(Thread t, Message m);
+        public AllEmails getAllThreads(User u);
+        public AllEmails getAllSentThreads(User u);
+        public AllEmails getAllMessages(AllEmails b);
+        public List<Message> getAllMessages(Thread t);
+        public AllEmails getAllSentEmails(AllEmails a);
+        public AllEmails GetallEmails(AllEmails a);
+    }
+}
---
+++ Models/Interface/IUserRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Models.Interface
+{
+    public interface IUserRepo
+    {
+        public bool addUser(User u);
+        public bool verifyUser(User u);
+        public bool uniqueEmail(User us);
+    }
+}
